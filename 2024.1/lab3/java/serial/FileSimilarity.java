90c99a55f7662dce6d1123196322518ebde3ec50
Iury Barbosa <iury_barbosa07@hotmail.com>
2024-09-10 12:31:13 -0300

    corrigindo lab3

--- 2024.1/lab3/java/serial/FileSimilarity.java
+++ 2024.1/lab3/java/serial/FileSimilarity.java
@@ -12,11 +12,21 @@ public class FileSimilarity {
         // Create a map to store the fingerprint for each file
         Map<String, List<Long>> fileFingerprints = new HashMap<>();
 
+        Map<String, Thread> threads = new HashMap<>();
+
         // Calculate the fingerprint for each file
         for (String path : args) {
-            List<Long> fingerprint = fileSum(path);
-            fileFingerprints.put(path, fingerprint);
+            Thread thread = Thread.ofVirtual().unstarted(new FileSum(path, fileFingerprints));
+            threads.put(path, thread);
+            thread.start();
         }
+
+        for (String path : args){
+            threads.get(path).join();
+        }
+
+        Map<String, Thread> similarityThreads = new HashMap<>();
+
 
         // Compare each pair of files
         for (int i = 0; i < args.length; i++) {
@@ -25,45 +35,97 @@ public class FileSimilarity {
                 String file2 = args[j];
                 List<Long> fingerprint1 = fileFingerprints.get(file1);
                 List<Long> fingerprint2 = fileFingerprints.get(file2);
-                float similarityScore = similarity(fingerprint1, fingerprint2);
-                System.out.println("Similarity between " + file1 + " and " + file2 + ": " + (similarityScore * 100) + "%");
-            }
-        }
-    }
+                Thread threadSimilarity = Thread.ofVirtual().start(new Similarity(file1, file2, fingerprint1, fingerprint2));              
+                similarityThreads.put(file1 + "-" + file2, threadSimilarity);
 
-    private static List<Long> fileSum(String filePath) throws IOException {
-        File file = new File(filePath);
-        List<Long> chunks = new ArrayList<>();
-        try (FileInputStream inputStream = new FileInputStream(file)) {
-            byte[] buffer = new byte[100];
-            int bytesRead;
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                long sum = sum(buffer, bytesRead);
-                chunks.add(sum);
-            }
-        }
-        return chunks;
-    }
-
-    private static long sum(byte[] buffer, int length) {
-        long sum = 0;
-        for (int i = 0; i < length; i++) {
-            sum += Byte.toUnsignedInt(buffer[i]);
-        }
-        return sum;
-    }
-
-    private static float similarity(List<Long> base, List<Long> target) {
-        int counter = 0;
-        List<Long> targetCopy = new ArrayList<>(target);
-
-        for (Long value : base) {
-            if (targetCopy.contains(value)) {
-                counter++;
-                targetCopy.remove(value);
             }
         }
 
-        return (float) counter / base.size();
+        // Wait for all similarity threads to finish
+        for (Thread thread : similarityThreads.values()) {
+            thread.join();
+        }
+    }
+
+    public static class FileSum implements Runnable {
+
+        private final String path;
+        private Map<String, List<Long>> fileFingerprints = new HashMap<>();
+
+
+        public FileSum(String path, Map<String, List<Long>> fileFingerprints){
+            this.fileFingerprints = fileFingerprints;
+            this.path = path;
+        }
+
+        @Override
+        public void run() {
+            try {
+                List<Long> fingerprint = fileSum(this.path);
+                fileFingerprints.put(path, fingerprint);
+            } catch (IOException e) {
+            }
+        }
+
+        private static List<Long> fileSum(String filePath) throws IOException {
+            File file = new File(filePath);
+            List<Long> chunks = new ArrayList<>();
+            try (FileInputStream inputStream = new FileInputStream(file)) {
+                byte[] buffer = new byte[100];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    long sum = sum(buffer, bytesRead);
+                    chunks.add(sum);
+                }
+            }
+            return chunks;
+        }
+    
+        private static long sum(byte[] buffer, int length) {
+            long sum = 0;
+            for (int i = 0; i < length; i++) {
+                sum += Byte.toUnsignedInt(buffer[i]);
+            }
+            return sum;
+        }
+    }
+
+    public static class Similarity implements Runnable {
+
+        private final String file1;
+        private final String file2;
+        private final List<Long> base;
+        private final List<Long> target;
+
+        public Similarity(String file1, String file2, List<Long> base, List<Long> target){
+            this.base = base;
+            this.target = target;
+            this.file1 = file1;
+            this.file2 = file2;
+        }
+
+        @Override
+        public void run() {
+            try {
+                float similarityScore = similarity(base, target);
+                System.out.println("Similarity between " + this.file1 + " and " + this.file2 + ": " + (similarityScore * 100) + "%");
+            } catch (IOException e) {
+            }
+        }
+
+        private static float similarity(List<Long> base, List<Long> target) throws IOException{
+    
+            int counter = 0;
+            List<Long> targetCopy = new ArrayList<>(target);
+    
+            for (Long value : base) {
+                if (targetCopy.contains(value)) {
+                    counter++;
+                    targetCopy.remove(value);
+                }
+            }
+    
+            return (float) counter / base.size();
+        }
     }
 }
